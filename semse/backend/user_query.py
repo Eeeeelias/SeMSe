fixing bugs with the similarity calculation

semse/backend/user_query.py
@@ -9,10 +9,12 @@ def combine_parts(query_result, title, episode_id, timestamp=None):
     query_result.sort(key=lambda x: x[-1] if x[-1] is not None else 0)
 
     combined_text = ""
+    embeddings = []
     for k in query_result:
         if k[0] == title and k[1] == episode_id and (timestamp is None or k[3] == timestamp):
-            combined_text += k[4]
-    return combined_text
+            combined_text += k[4] + " "
+            embeddings.append(eval(k[-2]))
+    return combined_text, embeddings
 
 
 def combine_multi_part_query(query_result: list, type=None) -> list:
@@ -24,18 +26,20 @@ def combine_multi_part_query(query_result: list, type=None) -> list:
         for q in query_result:
             title, episode_id, language, timestamp, text, embedding, part = q
             if part is None:
-                combined.append(q)
-            else:
-                combined_text = combine_parts(query_result, title, episode_id, timestamp)
-                combined.append((title, episode_id, language, timestamp, combined_text, embedding, None))
+                embedding = eval(embedding)
+                combined.append((title, episode_id, language, timestamp, text, [embedding], None))
+            elif part == 0:
+                combined_text, embeddings = combine_parts(query_result, title, episode_id, timestamp)
+                combined.append((title, episode_id, language, timestamp, combined_text, embeddings, None))
     elif type == 'description':
         for q in query_result:
             title, episode_id, text, embedding, part = q
             if part is None:
-                combined.append(q)
+                embedding = eval(embedding)
+                combined.append((title, episode_id, text, [embedding], None))
             else:
-                combined_text = combine_parts(query_result, title, episode_id)
-                combined.append((title, episode_id, combined_text, embedding, None))
+                combined_text, embeddings = combine_parts(query_result, title, episode_id)
+                combined.append((title, episode_id, combined_text, embeddings, None))
     return combined
 
 
@@ -47,7 +51,7 @@ def format_episode_id(episode_id: str):
 
 
 def query_db(query: str, show: str = None, table: str = None,
-             language: str = 'English', season=None, type='both', offset=0, conn=None):
+             language: str = None, season=None, type='both', offset=0, conn=None):
     if not conn:
         conn = dbf.get_conn()
     else:
@@ -66,7 +70,8 @@ def query_db(query: str, show: str = None, table: str = None,
         results_desc = description_query(*general_data, len(results_sub), limit=5, offset=offset, season=season)
         results = {**results_desc, **results_sub}
 
-    # query descriptions as well
+    # sort results by similarity
+    results = {k: v for k, v in sorted(results.items(), key=lambda item: item[1]['similarity'], reverse=True)}
     return results
 
 
@@ -77,8 +82,8 @@ def subtitle_query(conn, embed_query: np.ndarray, show: str = None,
     results_sub = combine_multi_part_query(results_sub, 'conversation')
     results = {}
     for idx, result in enumerate(results_sub):
-        title, episode_id, _, timestamp, text, embedding, _ = result
-        similarity = compute_cosine_similarity(embed_query, eval(embedding))
+        title, episode_id, _, timestamp, text, embeddings, _ = result
+        similarity = max([compute_cosine_similarity(x, embed_query) for x in embeddings])
         offset = int(offset)
         results[idx + offset] = {'title': title, 'episode_id': format_episode_id(episode_id), 'timestamp': timestamp,
                                  'text': text, 'similarity': similarity, 'type': 'conversation'}