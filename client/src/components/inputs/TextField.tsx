4b72c561d007add7c51ef8711b638f145cf87b55
PrettyCoffee <jonathan-holz@outlook.com>
2024-05-16 22:07:36 +0200

    refactor(client/text-input): Extract TextField

---
+++ client/src/components/inputs/TextField.tsx
@@ -0,0 +1,69 @@
+import { useAtomValue } from "@yaasl/preact"
+import { forwardRef } from "preact/compat"
+import { Dispatch, useMemo, useState } from "preact/hooks"
+
+import { breakpoint } from "~/data/breakpoint"
+import { cn } from "~/utils/cn"
+import { meassureText } from "~/utils/meassureText"
+import { useMergeRefs } from "~/utils/mergeRefs"
+
+import { ClassNameProp, FocusHandlerProps } from "../base/BaseProps"
+
+export interface TextFieldProps extends FocusHandlerProps, ClassNameProp {
+  value?: string
+  placeholder?: string
+  onChange?: Dispatch<string>
+  isValid?: (value: string) => boolean
+}
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  (
+    { className, value, placeholder = "", onChange, onBlur, onFocus, isValid },
+    externalRef
+  ) => {
+    const { isMobile } = useAtomValue(breakpoint)
+    const [text, setText] = useState(value ?? "")
+    const [internalRef, setInternalRef] = useState<HTMLInputElement | null>(
+      null
+    )
+    const ref = useMergeRefs([setInternalRef, externalRef])
+
+    const width = useMemo(() => {
+      if (!internalRef || isMobile) return
+      const placeholderWidth = meassureText(placeholder, internalRef).width
+      const valueWidth = meassureText(text, internalRef).width
+      return Math.max(placeholderWidth, valueWidth)
+    }, [placeholder, text, internalRef, isMobile])
+
+    //useEffect(() => {
+    //  if (!internalRef || internalRef.value === value) return
+    //  internalRef.value = value ?? ""
+    //}, [internalRef, value])
+
+    return (
+      <input
+        ref={ref}
+        type="text"
+        className={cn(
+          "text-text-priority placeholder:text-text-gentle/50 size-full min-w-[5ch] max-w-[30ch] rounded bg-transparent outline-none",
+          className
+        )}
+        value={text}
+        placeholder={placeholder}
+        onInput={event => {
+          const value = event.currentTarget.value
+          if (isValid && !isValid(value)) {
+            event.currentTarget.value = text
+            return
+          }
+
+          setText(value)
+          onChange?.(value)
+        }}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        style={{ width }}
+      />
+    )
+  }
+)
--- client/src/components/inputs/TextInput.tsx
+++ client/src/components/inputs/TextInput.tsx
@@ -1,20 +1,13 @@
 import { useAtomValue } from "@yaasl/preact"
 import { forwardRef } from "preact/compat"
-import { Dispatch, useMemo, useState } from "preact/hooks"
 
 import { breakpoint } from "~/data/breakpoint"
 import { cn } from "~/utils/cn"
-import { meassureText } from "~/utils/meassureText"
-import { useMergeRefs } from "~/utils/mergeRefs"
 
 import { Decorator, AlertProps, Action } from "./Decorator"
-import { ClassNameProp, FocusHandlerProps } from "../base/BaseProps"
+import { TextField, TextFieldProps } from "./TextField"
 
-export interface TextInputProps extends FocusHandlerProps, ClassNameProp {
-  value?: string
-  placeholder?: string
-  onChange?: Dispatch<string>
-  isValid?: (value: string) => boolean
+export interface TextInputProps extends TextFieldProps {
   label: string
   alert?: AlertProps
   action?: Action
@@ -33,47 +26,22 @@ export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
       action,
       ...labelProps
     },
-    externalRef
+    ref
   ) => {
     const { isMobile } = useAtomValue(breakpoint)
-    const [text, setText] = useState(value ?? "")
-    const [internalRef, setInternalRef] = useState<HTMLInputElement | null>(
-      null
-    )
-    const ref = useMergeRefs([setInternalRef, externalRef])
-
-    const width = useMemo(() => {
-      if (!internalRef || isMobile) return
-      const placeholderWidth = meassureText(placeholder, internalRef).width
-      const valueWidth = meassureText(text, internalRef).width
-      return Math.max(placeholderWidth, valueWidth)
-    }, [placeholder, text, internalRef, isMobile])
 
     return (
       <Decorator.Border action={action} className={cn(isMobile && "w-56")}>
         <Decorator.Label {...labelProps} className={"cursor-text"}>
-          <input
+          <TextField
             ref={ref}
-            type="text"
-            className={cn(
-              "text-text-priority placeholder:text-text-gentle/50 size-full min-w-[5ch] max-w-[30ch] rounded bg-transparent outline-none",
-              className
-            )}
-            value={text}
+            value={value}
             placeholder={placeholder}
-            onInput={event => {
-              const value = event.currentTarget.value
-              if (isValid && !isValid(value)) {
-                event.currentTarget.value = text
-                return
-              }
-
-              setText(value)
-              onChange?.(value)
-            }}
+            onChange={onChange}
             onBlur={onBlur}
             onFocus={onFocus}
-            style={{ width }}
+            className={className}
+            isValid={isValid}
           />
         </Decorator.Label>
       </Decorator.Border>
