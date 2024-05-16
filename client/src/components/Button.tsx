1d22acdc585e3dbc0bd94347b18e7e51de3bfd84
PrettyCoffee <jonathan-holz@outlook.com>
2024-05-16 22:48:06 +0200

    feat(client/button): Add disabled prop

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -9,29 +9,32 @@ import {
   FocusHandlerProps,
 } from "./base/BaseProps"
 
-const button = cva("inline-flex items-center truncate whitespace-nowrap", {
-  variants: {
-    kind: {
-      key: "text-text-surface bgl-base-background-surface hover:bgl-layer-b/10 focus-visible:bgl-layer-b/10 active:bgl-layer-b/20",
-      ghost:
-        "text-text bgl-base-transparent hover:bgl-layer-w/5 focus-visible:bgl-layer-w/5 active:bgl-layer-w/10 border-text-muted border",
-      flat: "text-text bgl-base-transparent hover:bgl-layer-w/5 focus-visible:bgl-layer-w/5 active:bgl-layer-w/10",
+const button = cva(
+  "inline-flex items-center truncate whitespace-nowrap disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      kind: {
+        key: "text-text-surface bgl-base-background-surface hover:bgl-layer-b/10 focus-visible:bgl-layer-b/10 active:bgl-layer-b/20",
+        ghost:
+          "text-text bgl-base-transparent hover:bgl-layer-w/5 focus-visible:bgl-layer-w/5 active:bgl-layer-w/10 border-text-muted border",
+        flat: "text-text bgl-base-transparent hover:bgl-layer-w/5 focus-visible:bgl-layer-w/5 active:bgl-layer-w/10",
+      },
+      round: {
+        true: "rounded-full",
+        false: "rounded",
+      },
+      size: {
+        default: "h-10 shrink-0 gap-2 px-3",
+        icon: "size-10 shrink-0 items-center justify-center",
+      },
     },
-    round: {
-      true: "rounded-full",
-      false: "rounded",
+    defaultVariants: {
+      kind: "flat",
+      round: false,
+      size: "default",
     },
-    size: {
-      default: "h-10 shrink-0 gap-2 px-3",
-      icon: "size-10 shrink-0 items-center justify-center",
-    },
-  },
-  defaultVariants: {
-    kind: "flat",
-    round: false,
-    size: "default",
-  },
-})
+  }
+)
 
 export interface ButtonProps
   extends VariantProps<typeof button>,
@@ -40,6 +43,7 @@ export interface ButtonProps
     ChildrenProp {
   onClick?: () => void
   tabIndex?: number
+  disabled?: boolean
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
