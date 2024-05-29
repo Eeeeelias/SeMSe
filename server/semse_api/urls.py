fdde0095fd02191bd92c062fef9878c408ff9156
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-05-29 10:14:15 +0200

    feat(server/api): adapted /api endpoint for singular subdomain host

--- server/semse_api/urls.py
+++ server/semse_api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('media/', views.get_all_media, name='get_all_media'),
-    path('query/', views.query_media, name='query_media'),
-    path('queryPlain/', views.query_media_fts, name='query_media_fts'),
-    path('size/', views.get_media_size, name='get_media_size'),
-    path('test/', views.test_api, name='test'),
-    path('image/<str:uuid>/', views.serve_image, name='serve_image'),
+    path('api/media/', views.get_all_media, name='get_all_media'),
+    path('api/query/', views.query_media, name='query_media'),
+    path('api/queryPlain/', views.query_media_fts, name='query_media_fts'),
+    path('api/size/', views.get_media_size, name='get_media_size'),
+    path('api/test/', views.test_api, name='test'),
+    path('api/image/<str:uuid>/', views.serve_image, name='serve_image'),
 ]
