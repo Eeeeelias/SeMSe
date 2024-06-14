615be2beaa0296979ce9c189f1ec5ea18721d9fd
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2024-06-14 22:42:56 +0200

    feat(client/results): Make result lists expandable

--- client/src/page/results/ResultList.tsx
+++ client/src/page/results/ResultList.tsx
@@ -1,5 +1,6 @@
-import { Dispatch } from "preact/hooks"
+import { useState, Dispatch } from "preact/hooks"
 
+import { Icon } from "~/components/base/Icon"
 import { QueryResult } from "~/generated-api"
 
 import { MatchCard } from "./MatchCard"
@@ -8,24 +9,53 @@ const getId = (result: QueryResult[number]) =>
   (result.title ?? "") + (result.episodeId ?? "") + (result.timestamp ?? "")
 
 interface MatchListProps {
+  title: string
   highlight?: string
   matches: QueryResult
   onOpenDetails: Dispatch<QueryResult[number]>
 }
 
 export const ResultList = ({
+  title,
   highlight,
   matches,
   onOpenDetails,
-}: MatchListProps) => (
-  <div className="grid grid-cols-[repeat(auto-fit,minmax(theme(width.60),1fr))] justify-center gap-4">
-    {matches.map(result => (
-      <MatchCard
-        key={getId(result)}
-        {...result}
-        highlight={highlight}
-        onOpenDetails={() => onOpenDetails(result)}
-      />
-    ))}
-  </div>
-)
+}: MatchListProps) => {
+  const expandable = matches.length > 4
+  const [expanded, setExpanded] = useState(false)
+  const displayedMatches = expanded ? matches : matches.slice(0, 4)
+
+  const headline = (
+    <h2 className="text-xl font-bold">
+      {title}
+      <span className="text-text-gentle ml-4 text-sm">
+        {matches.length} matches
+      </span>
+    </h2>
+  )
+  return (
+    <>
+      {expandable ? (
+        <button
+          onClick={() => setExpanded(state => !state)}
+          className="bgl-base-transparent hover:bgl-layer-w/5 focus-visible:bgl-layer-w/5 active:bgl-layer-w/10 mb-2 mt-6 flex items-center gap-2 rounded px-3 py-2"
+        >
+          <Icon icon={expanded ? "caret-down" : "caret-right"} />
+          {headline}
+        </button>
+      ) : (
+        <div className="mb-4 mt-8">{headline}</div>
+      )}
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(theme(width.60),1fr))] justify-center gap-4">
+        {displayedMatches.map(result => (
+          <MatchCard
+            key={getId(result)}
+            {...result}
+            highlight={highlight}
+            onOpenDetails={() => onOpenDetails(result)}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
--- client/src/page/results/Results.tsx
+++ client/src/page/results/Results.tsx
@@ -18,27 +18,23 @@ export const Results = ({ results }: ResultsProps) => {
   return (
     <>
       {results.plain.length > 0 && (
-        <>
-          <h2 className="mb-4 mt-8 text-xl font-bold">Plain matches</h2>
-          <ResultList
-            highlight={results.query}
-            matches={results.plain}
-            onOpenDetails={data =>
-              setSelected({ highlight: results.query, data })
-            }
-          />
-        </>
+        <ResultList
+          title="Text search"
+          highlight={results.query}
+          matches={results.plain}
+          onOpenDetails={data =>
+            setSelected({ highlight: results.query, data })
+          }
+        />
       )}
       {results.llm.length > 0 && (
-        <>
-          <h2 className="mb-4 mt-8 text-xl font-bold">LLM matches</h2>
-          <ResultList
-            matches={results.llm}
-            onOpenDetails={data =>
-              setSelected({ highlight: data.exactMatch ?? "", data })
-            }
-          />
-        </>
+        <ResultList
+          title="Semantic search"
+          matches={results.llm}
+          onOpenDetails={data =>
+            setSelected({ highlight: data.exactMatch ?? "", data })
+          }
+        />
       )}
       {selected && (
         <DetailsModal
